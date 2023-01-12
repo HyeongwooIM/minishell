67b80fccc4d7ad59cb2bbd942c65bb6e10ace925
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-01-12 15:54:20 +0900

    ft_unset 추가

--- execute/ft_unset.c
+++ execute/ft_unset.c
@@ -1,1 +1,28 @@
 #include "../minishell.h"
+
+void ft_unset(t_cmd *cmd)
+{
+	t_env	*env_temp;
+	t_env	*del_temp;
+
+	env_temp = g_info->env_list;
+	if (env_temp && ft_strcmp(env_temp->key, cmd->content))
+	{
+		g_info->env_list = env_temp->next;
+		free(env_temp->key);
+		free(env_temp->value);
+		free(env_temp);
+	}
+	else
+	{
+		while (env_temp->next && ft_strcmp(env_temp->next->key, cmd->content))
+			env_temp = env_temp->next;
+	}
+	if (!env_temp->next)
+		exit(1); // not a valid identifier
+	del_temp = env_temp->next;
+	env_temp->next = del_temp->next;
+	free(del_temp->key);
+	free(del_temp->value);
+	free(del_temp);
+}
