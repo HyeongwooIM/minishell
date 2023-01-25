cbb897037c48b60a35730651ef122bc6666ad8ef
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-01-25 14:22:58 +0900

    에러처리 수정

--- util/ft_error_exit.c
+++ util/ft_error_exit.c
@@ -8,5 +8,14 @@ void ft_error_return(char *str, int error_no)
 void ft_error_exit(char *str, int error_no)
 {
 	write(2, str, ft_strlen(str));
+	write(2, "\n", 1);
 	exit(error_no);
+}
+
+void	ft_command_error(char *cmd)
+{
+	ft_putstr_fd(cmd, 2);
+	ft_putstr_fd(": command not found\n", 2);
+	g_info.last_exit_num = 127;
+	exit(127);
 }
