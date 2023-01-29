7da6231d137acfbd47fb0c4e90245980c9e26c0a
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-01-29 16:38:36 +0900

    unset도 export처럼 수정

--- src/execute/ft_pwd.c
+++ src/execute/ft_pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: woohyeong <woohyeong@student.42.fr>        +#+  +:+       +#+        */
+/*   By: him <him@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 18:37:51 by him               #+#    #+#             */
-/*   Updated: 2023/01/28 23:24:34 by woohyeong        ###   ########.fr       */
+/*   Updated: 2023/01/29 16:29:32 by him              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@ void	ft_pwd(void)
 		exit (1);
 	}
 	printf("%s\n", get_cwd);
+	g_info.last_exit_num = 0;
 }
